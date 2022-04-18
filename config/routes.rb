5d37963109754f579635170f3eacd74b69c83c60
Jacques Chester <jacques.chester@shopify.com>
2022-04-18 10:11:52 -0400

    Remove autogenerated comment from routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,4 @@ Rails.application.routes.draw do
   resources :estimates
   resources :projects
   resources :experts
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
