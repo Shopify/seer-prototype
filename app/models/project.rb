b3c3f8be91834b98bdcb7e3c6d231ad19b3733d6
Jacques Chester <jacques.chester@shopify.com>
2022-04-21 11:29:04 -0400

    Extract Project view lookups to model

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,28 @@ class Project < ApplicationRecord
   has_many :estimates
 
   validates :name, presence: true
+
+  def min_of_min_magnitudes
+    self.estimates.minimum(:min_magnitude)
+  end
+
+  def average_of_likely_magnitudes
+    self.estimates.average(:likely_magnitude)
+  end
+
+  def max_of_max_magnitudes
+    self.estimates.maximum(:max_magnitude)
+  end
+
+  def min_of_min_frequencies
+    self.estimates.minimum(:min_frequency)
+  end
+
+  def average_of_likely_frequencies
+    self.estimates.average(:likely_frequency)
+  end
+
+  def max_of_max_frequencies
+    self.estimates.maximum(:max_frequency)
+  end
 end
