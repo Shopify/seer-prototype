fba53d5b866712e7b420bb9946af5293bf60c7eb
Jacques Chester <jacques.chester@shopify.com>
2022-04-25 15:05:58 -0400

    Convert liklely freq. to range bounded by min/max
    Also convert min/max freq. fields from text to number

--- app/javascript/controllers/estimate_controller.js
+++ app/javascript/controllers/estimate_controller.js
@@ -1,10 +1,14 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["minMagnitude", "likelyMagnitude", "maxMagnitude", "likelyMagnitudeOutput"]
+  static targets = [
+    "minMagnitude", "likelyMagnitude", "maxMagnitude", "likelyMagnitudeOutput",
+    "minFrequency", "likelyFrequency", "maxFrequency", "likelyFrequencyOutput",
+  ]
 
   connect() {
     this.updateLikelyMagnitude()
+    this.updateLikelyFrequency()
   }
 
   updateLikelyMagnitude() {
@@ -12,6 +16,17 @@ export default class extends Controller {
     const likely = this.likelyMagnitudeTarget
     const output = this.likelyMagnitudeOutputTarget
     const max = this.maxMagnitudeTarget
+
+    likely.min = min.value
+    likely.max = max.value
+    output.value = likely.value
+  }
+
+  updateLikelyFrequency() {
+    const min = this.minFrequencyTarget
+    const likely = this.likelyFrequencyTarget
+    const output = this.likelyFrequencyOutputTarget
+    const max = this.maxFrequencyTarget
 
     likely.min = min.value
     likely.max = max.value
